a01a82faf88313a32bfc526f1ed9dd7bc31bdd54
SalvadorM <josesalvador0102@gmail.com>
2025-02-08 17:49:44 +0000

    up

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,28 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Habilitar CORS
   app.enableCors({
-    origin: 'https://edgetrader.vercel.app',
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type'],
-  });  
+    origin: '*', // Cambia a 'https://edgetrader.vercel.app' cuando funcione
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
+  // Middleware para manejar preflight requests (CORS)
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization");
+
+    if (req.method === "OPTIONS") {
+      return res.sendStatus(200);
+    }
+    next();
+  });
+
   await app.listen(3000);
   setInterval(() => console.log("Server is running..."), 300000);
 }
+
 bootstrap();
