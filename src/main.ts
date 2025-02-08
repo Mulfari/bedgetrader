438b94939c8c8ef0426f57cf032289623ce258be
SalvadorM <josesalvador0102@gmail.com>
2025-02-08 16:58:11 +0000

    up

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   app.enableCors({
-    origin: '*', // O mejor: 'https://edgetrader.vercel.app'
+    origin: 'https://edgetrader.vercel.app', // Reemplaza con la URL de tu frontend en Vercel
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type'],
   });
-
   await app.listen(3000);
 }
 bootstrap();
