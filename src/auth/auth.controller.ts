cf1f09863d77c87e894c85053ce2a119c3e0da34
SalvadorM <josesalvador0102@gmail.com>
2025-02-09 00:25:05 +0000

    up

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,32 +1,35 @@
-// auth.controller.ts
 import { Body, Controller, HttpException, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private jwtService: JwtService
+  ) {}
 
   @Post('register')
   async register(@Body() body: { name: string; email: string; password: string }) {
     try {
       const user = await this.authService.registerUser(body.name, body.email, body.password);
-      console.log("✅ Usuario creado exitosamente:", user);
-      return user;
+      return { message: "Usuario creado exitosamente", userId: user.id };
     } catch (error) {
-      console.error("❌ Error al crear usuario:", error);
-      throw error;
+      console.error("Error al crear usuario:", error);
+      throw new HttpException('Error registering user', HttpStatus.BAD_REQUEST);
     }
   }
 
   @Post('login')
   async login(@Body() body: { email: string; password: string }) {
-    try {
-      const { email, password } = body;
-      const result = await this.authService.validateUser(email, password);
-      console.log("✅ Inicio de sesión exitoso:", result);
-      return { message: 'Autenticación exitosa', ...result };
-    } catch (error) {
-      console.error("❌ Error de autenticación:", error);
+    const user = await this.authService.validateUser(body.email, body.password);
+    if (user) {
+      const payload = { email: user.email, sub: user.id };
+      return {
+        message: 'Autenticación exitosa',
+        access_token: this.jwtService.sign(payload),
+      };
+    } else {
       throw new HttpException('Credenciales inválidas', HttpStatus.UNAUTHORIZED);
     }
   }
