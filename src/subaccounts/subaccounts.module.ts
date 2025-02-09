a049dd75606a8d1b5646fe92ead67a4d98729a3e
SalvadorM <josesalvador0102@gmail.com>
2025-02-09 18:33:04 +0000

    up

--- src/subaccounts/subaccounts.module.ts
+++ src/subaccounts/subaccounts.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SubaccountsService } from './subaccounts.service';
 import { SubaccountsController } from './subaccounts.controller';
+import { PrismaService } from "../prisma.service";
 
 @Module({
-  providers: [SubaccountsService],
+  providers: [SubaccountsService, PrismaService],
   controllers: [SubaccountsController]
 })
 export class SubaccountsModule {}
