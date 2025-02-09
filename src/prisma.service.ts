4d211b123bd1d76b604cb0170f5dbe7d626a8ad0
SalvadorM <josesalvador0102@gmail.com>
2025-02-09 18:28:28 +0000

    up

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,13 +1,15 @@
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Injectable, OnModuleInit, OnModuleDestroy } from "@nestjs/common";
+import { PrismaClient } from "@prisma/client";
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   async onModuleInit() {
+    console.log("🔌 Conectando a la base de datos...");
     await this.$connect();
   }
 
   async onModuleDestroy() {
+    console.log("⚡ Desconectando de la base de datos...");
     await this.$disconnect();
   }
 }
