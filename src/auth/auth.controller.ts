00bd49d316f3cd8cb79aea6685c4586c89bcd16c
SalvadorM <josesalvador0102@gmail.com>
2025-02-09 00:10:58 +0000

    up

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,5 @@
 // auth.controller.ts
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpException, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
@@ -15,6 +15,19 @@ export class AuthController {
     } catch (error) {
       console.error("❌ Error al crear usuario:", error);
       throw error;
+    }
+  }
+
+  @Post('login')
+  async login(@Body() body: { email: string; password: string }) {
+    try {
+      const { email, password } = body;
+      const result = await this.authService.validateUser(email, password);
+      console.log("✅ Inicio de sesión exitoso:", result);
+      return { message: 'Autenticación exitosa', ...result };
+    } catch (error) {
+      console.error("❌ Error de autenticación:", error);
+      throw new HttpException('Credenciales inválidas', HttpStatus.UNAUTHORIZED);
     }
   }
 }
