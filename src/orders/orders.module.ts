ae9c0d7e4152f77f94ba860887abd044e1ca12e6
SalvadorM <josesalvador0102@gmail.com>
2025-03-09 20:59:23 +0000

    asd

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
 import { PrismaModule } from '../prisma.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    AuthModule,
+  ],
   controllers: [OrdersController],
   providers: [OrdersService],
   exports: [OrdersService],
