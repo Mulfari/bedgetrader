eeb795557f48d7f1c6f1544525b80fc655279026
SalvadorM <josesalvador0102@gmail.com>
2025-03-12 15:34:01 +0000

    asd

--- src/positions/positions.module.ts
+++ src/positions/positions.module.ts
@@ -1,15 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PositionsService } from './positions.service';
-import { PositionsController } from './positions.controller';
 import { ConfigModule } from '@nestjs/config';
-import { PrismaService } from '../prisma.service';
 
 @Module({
   imports: [
     ConfigModule,
   ],
-  controllers: [PositionsController],
-  providers: [PositionsService, PrismaService],
+  providers: [PositionsService],
   exports: [PositionsService],
 })
 export class PositionsModule {} 
