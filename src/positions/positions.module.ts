80dd7c33bba672042eb26ee838e709fb2edb2c65
SalvadorM <josesalvador0102@gmail.com>
2025-03-12 16:27:28 +0000

    asd

--- src/positions/positions.module.ts
+++ src/positions/positions.module.ts
@@ -1,15 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PositionsService } from './positions.service';
-import { PositionsController } from './positions.controller';
 import { ConfigModule } from '@nestjs/config';
-import { SubaccountsModule } from '../subaccounts/subaccounts.module';
 
 @Module({
   imports: [
     ConfigModule,
-    SubaccountsModule,
   ],
-  controllers: [PositionsController],
   providers: [PositionsService],
   exports: [PositionsService],
 })
